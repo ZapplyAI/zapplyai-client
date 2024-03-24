415a51fc918baced113ad56d2b25cca96d9230fa
weareandrei <weareandrei@gmail.com>
2024-03-24 12:30:05 +0000

    User accounts frontend v1.
    No dark theme.
    No Google auth.

---
+++ components/Authentication/SignUp.tsx
@@ -0,0 +1,98 @@
+import React from "react"
+import {Button, Grid, Link, TextField} from "@mui/material"
+
+import OpenInNewIcon from '@mui/icons-material/OpenInNew'
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch'
+import SettingsIcon from '@mui/icons-material/Settings'
+
+const SignUp = (): React.ReactNode => {
+    return (
+      <div style={style.formContainer}>
+          <form noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              {/*<FormControlLabel*/}
+              {/*  control={<Checkbox value="remember" color="primary" />}*/}
+              {/*  label="Remember me"*/}
+              {/*/>*/}
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                style={style.submitButton}
+              >
+                  Sign Up
+              </Button>
+              <Grid container>
+                  <Grid item xs>
+                      <Link href="#" variant="body2">
+                          Forgot password?
+                      </Link>
+                  </Grid>
+                  <Grid item>
+                      <Link href="/login" variant="body2">
+                          {"Already have an account? Lon In"}
+                      </Link>
+                  </Grid>
+              </Grid>
+          </form>
+      </div>
+    )
+}
+
+const style = {
+    formContainer: {
+        width: '350px'
+    },
+    buttonContainer: {
+        // display: 'flex',
+        // flexDirection: 'column',
+        // justifyContent: 'start'
+    },
+    appsContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%'
+    },
+    navButton: {
+        padding: 0
+    },
+    navIcon: {
+        width: '22px',
+        color: '#CFCED9'
+    },
+    submitButton: {
+        background: '#282636',
+        color: '#CFCED9'
+    }
+}
+
+export default SignUp
--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -1,12 +1,70 @@
+"use client"
+
 import React from "react"
 import {Avatar, Logo} from "@/components"
+import {Button} from "@mui/material"
+import RightDrawer from "../RightDrawer/RightDrawer"
+import SignUp from "@/components/Authentication/SignUp"
+import LogIn from "@/components/Authentication/LogIn"
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+
+const loggedIn = false
 
 const Navbar = (): React.ReactNode => {
+    const [drawerOpened, setDrawerOpen] = React.useState("")
+
+    const closeDrawer = () => {
+        setDrawerOpen('')
+    }
+
+    const toggleSignUpDrawer = (open) => {
+        setDrawerOpen(open ? 'Sign Up' : '')
+    }
+
+    const toggleLogInDrawer = (open) => {
+        setDrawerOpen(open ? 'Log In' : '')
+    }
+
     return (
         <nav style={style.nav}>
             <Logo />
-            <Avatar />
+            {loggedIn ? (<Avatar />) : (
+              <div>
+                  <Button style={{ marginRight: "10px" }}
+                          onClick={toggleSignUpDrawer}>
+                      Sign Up
+                  </Button>
+                  <Button style={{ marginRight: "10px" }}
+                          onClick={toggleLogInDrawer}>
+                      Log In
+                  </Button>
+              </div>
+            )}
+
+            <div style={{ position: "absolute" }}>
+                <RightDrawer
+                  title={drawerOpened}
+                  icon={
+                        <div style={{
+                                ...style.headerIconStyle,
+                                borderRadius: 100,
+                                background: '#f50057'
+                            }}>
+                            <LockOpenIcon style={{
+                              ...style.headerIconStyle,
+                              color: '#FFFFFF',
+                              height: '65%',
+                              width: '65%'}}/>
+                        </div>
+                        }
+                  component={drawerOpened === 'Sign Up' ? <SignUp/> : <LogIn/> }
+                  isOpen={drawerOpened !== ''}
+                  closeDrawer={closeDrawer}
+                  />
+              </div>
         </nav>
+
+
     )
 }
 
@@ -20,7 +78,14 @@ const style = {
         padding: '0px 12px',
         background: '#181818',
         borderBottom: '1px solid #282636'
-    }
+    },
+  headerIconStyle: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%'
+  }
 }
 
 export default Navbar
