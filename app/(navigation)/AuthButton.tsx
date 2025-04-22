f2c76e35635a429df85ce67deb483ae8fc328071
weareandrei <weareandrei@gmail.com>
2025-04-22 22:41:57 +0300

    Fixed Sign In Button

--- app/(navigation)/AuthButton.tsx
+++ app/(navigation)/AuthButton.tsx
@@ -1,45 +1,72 @@
-import Button from '@mui/material/Button'
 import React from 'react'
-import Link from 'next/link'
 import { auth0 } from '@/lib/auth0'
+import ClippedButton from '@/app/(components)/ClippedButton'
 
 const AuthButton = async () => {
   const session = await auth0.getSession()
 
   if (!session) {
     return (
-      <a href={'/auth/login'}>
-        <Button
-          variant="contained"
+      <a href="/auth/login">
+        <ClippedButton
+          // onClick={() => router.push('/auth/login')}
           sx={{
-            marginLeft: 'auto',
-            background: '#775EFF',
-            '&:hover': {
-              background: '#5E3FFF',
-            },
+            fontFamily: 'Tektur, sans-serif',
+            fontSize: '0.9rem',
+            padding: '8px 16px',
+            marginRight: '15px',
           }}
         >
-          Try now
-        </Button>
+          Sign In
+        </ClippedButton>
       </a>
+
+      // <a href={'/auth/login'}>
+      //   <Button
+      //     variant="contained"
+      //     sx={{
+      //       marginLeft: 'auto',
+      //       background: '#775EFF',
+      //       '&:hover': {
+      //         background: '#5E3FFF',
+      //       },
+      //     }}
+      //   >
+      //     Try now
+      //   </Button>
+      // </a>
     )
   }
 
   return (
-    <Link href={'/dashboard'}>
-      <Button
-        variant="contained"
+    <a href="/dashboard">
+      <ClippedButton
+        // onClick={() => router.push('/dashboard')}
         sx={{
-          marginLeft: 'auto',
-          background: '#775EFF',
-          '&:hover': {
-            background: '#5E3FFF',
-          },
+          fontFamily: 'Tektur, sans-serif',
+          fontSize: '0.9rem',
+          padding: '8px 16px',
+          marginRight: '15px',
         }}
       >
         Go To Dashboard
-      </Button>
-    </Link>
+      </ClippedButton>
+    </a>
+
+    // <Link href={'/dashboard'}>
+    //   <Button
+    //     variant="contained"
+    //     sx={{
+    //       marginLeft: 'auto',
+    //       background: '#775EFF',
+    //       '&:hover': {
+    //         background: '#5E3FFF',
+    //       },
+    //     }}
+    //   >
+    //     Go To Dashboard
+    //   </Button>
+    // </Link>
   )
 }
 
