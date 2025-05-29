992a86f85272aa950fbab1d52d366213749d1d24
Israel Akintunde <israelakintunde005@gmail.com>
2025-05-29 01:29:31 +0100

    commit message

--- app/session/callback/route.ts
+++ app/session/callback/route.ts
@@ -14,5 +14,7 @@ export async function GET(req: Request) {
   const callbackUrl = queryParams.get('callback_url')
   queryParams.delete('callback_url')
 
-  return NextResponse.redirect(`${callbackUrl}?${queryParams}`)
+  return NextResponse.redirect(
+    `${callbackUrl!.split('://')[0]}://elastic-ai.elastic-code?${queryParams}`
+  )
 }
