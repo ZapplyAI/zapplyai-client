3ea68b91b2decd42edec28e2c0107e00f47f4d3d
Israel Akintunde <israelakintunde005@gmail.com>
2025-05-29 15:16:47 +0100

    Update extension URL to reflect accurate name and const assertion

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,1 @@
-export const EXTENSION_URL = 'https://marketplace.visualstudio.com/items?itemName=elastic-copilot.elastic-copilot' as string;
+export const EXTENSION_URL = 'https://marketplace.visualstudio.com/items?itemName=elastic-ai.elastic-code' as const;
